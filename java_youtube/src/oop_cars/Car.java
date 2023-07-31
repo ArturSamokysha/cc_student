c292f039a208130438402f4ea4a38ee45224ae42
Artur Samokysha <ASAM206@more2learn.de>
2023-07-31 12:13:38 +0200

    carsApp

--- java_youtube/src/oop_cars/Car.java
+++ java_youtube/src/oop_cars/Car.java
@@ -1,5 +1,46 @@
 package oop_cars;
 
 public class Car {
-
+	//fields
+	private String brand;
+	private int year;
+	private int price;
+	private boolean electric;
+	
+	//getter and setter
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public boolean isElectric() {
+		return electric;
+	}
+	public void setElectric(boolean electric) {
+		this.electric = electric;
+	}
+	
+	//constructor
+	public Car (String brand, int year, int price, boolean electric) {
+		this.brand = brand;
+		this.year = year;
+		this.price = price;
+		this.electric = electric;
+	}
+	
+	
+	
 }
--- java_youtube/src/oop_cars/CarsApp.java
+++ java_youtube/src/oop_cars/CarsApp.java
@@ -4,7 +4,18 @@ public class CarsApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Car id_1 = new Car("BMW", 2018, 25000, false);
+		Car id_2 = new Car("Tesla", 2022, 55000, true);
+		
+		System.out.println("Brand: " + id_1.getBrand() + 
+							"\nYear: " + id_1.getYear() +
+							"\nPrice: " + id_1.getPrice() +
+							"\nElectric: " + id_1.isElectric());
+		
+		System.out.println("\nBrand: " + id_2.getBrand() + 
+							"\nYear: " + id_2.getYear() +
+							"\nPrice: " + id_2.getPrice() +
+							"\nElectric: " + id_2.isElectric());
 	}
 
 }
